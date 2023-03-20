10fed78d9ab75ad6d0d7b317fdf583eb46eac9a5
yalu <114964251+ya1u@users.noreply.github.com>
2023-03-20 18:13:23 +0900

    haha l /

--- flighter-backend/src/main/java/com/green/flighter/config/SampleApplicationRunner.java
+++ flighter-backend/src/main/java/com/green/flighter/config/SampleApplicationRunner.java
@@ -34,7 +34,7 @@ public class SampleApplicationRunner implements ApplicationRunner {
 
                 Users user = Users.builder()
                         .id(1L)
-                        .email("poqwer95@naver.com")
+                        .email("yalu001@naver.com")
                         .password("todnqjrj1!")
                         .name("고광근")
                         .birth(LocalDate.of(1995, 7, 4))
